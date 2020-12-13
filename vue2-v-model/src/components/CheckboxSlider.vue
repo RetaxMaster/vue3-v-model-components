3f03b6247b3b007765c0589bd12d03fd1e60ad67
RetaxMaster <retaxmaster@hotmail.com>
2020-12-13 00:04:25 -0600

    Boilerplate

---
+++ vue2-v-model/src/components/CheckboxSlider.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="checkbox-slider">
+      <input type="checkbox" id="my-slider">
+      <label for="my-slider">Recordar contraseña</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxSlider'
+}
+</script>
+
+<style scoped>
+/* Checkbox Slider */
+
+.checkbox-slider input {
+    display: none;
+}
+
+.checkbox-slider label {
+    position: relative;
+    cursor: pointer;
+}
+
+.checkbox-slider label:after {
+    content: '';
+    display: inline-block;
+    width: 11px;
+    height: 11px;
+    position: absolute;
+    top: 3px;
+    left: calc(100% + 9px);
+    /*Porahora*/background: #fff;
+    border-radius: 20px;
+    transition: 0.2s all ease;
+    -webkit-border-radius: 20px;
+    -moz-border-radius: 20px;
+    -ms-border-radius: 20px;
+    -o-border-radius: 20px;
+    -webkit-transition: 0.2s all ease;
+    -moz-transition: 0.2s all ease;
+    -ms-transition: 0.2s all ease;
+    -o-transition: 0.2s all ease;
+}
+
+.checkbox-slider label:before {
+    content: '';
+    display: inline-block;
+    width: 30px;
+    height: 17px;
+    position: absolute;
+    left: calc(100% + 5px);
+    background: #ccc;
+    border-radius: 20px;
+    transition: 0.2s all ease;
+    -moz-border-radius: 20px;
+    -ms-border-radius: 20px;
+    -o-border-radius: 20px;
+    -webkit-transition: 0.2s all ease;
+    -moz-transition: 0.2s all ease;
+    -ms-transition: 0.2s all ease;
+    -o-transition: 0.2s all ease;
+}
+
+.checkbox-slider input:checked + label:after {
+    left: calc(100% + 20px);
+}
+
+.checkbox-slider input:checked + label:before {
+    background: #3d90da;
+}
+
+/* ->Checkbox Slider */
+</style>
---
+++ vue3-v-model/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+
+    <header>
+      Checkbox Slider
+    </header>
+
+    <div class="container">
+      <checkbox-slider />
+      <span>{{ remember_password }}</span>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import CheckboxSlider from './components/CheckboxSlider.vue'
+
+export default {
+
+  name: 'App',
+
+  components: { CheckboxSlider },
+
+  data() {
+    return {
+      remember_password: false
+    }
+  }
+
+}
+</script>
+
+<style>
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+html, body, #app {
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+}
+
+body {
+    font-family: "Lato";
+}
+
+header {
+    display: flex;
+    width: 100%;
+    background: #3d90da;
+    padding: 17px;
+    color: #fff;
+    font-weight: bold;
+}
+
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+}
+</style>
